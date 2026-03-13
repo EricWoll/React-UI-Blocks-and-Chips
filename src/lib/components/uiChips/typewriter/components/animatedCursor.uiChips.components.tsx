5e0d56ece21784da17926f2cf7539f71c961ad4c
RaZ <99991692+EricWoll@users.noreply.github.com>
2026-03-12 17:32:41 -0700

    Refactor AnimatedCursor component documentation

--- src/lib/components/uiChips/typewriter/components/animatedCursor.uiChips.components.tsx
+++ src/lib/components/uiChips/typewriter/components/animatedCursor.uiChips.components.tsx
@@ -1,5 +1,5 @@
-import '@/lib/components/uiChips/typewriter/styling/cursorBlink.typeWriter.css';
-import clsx from 'clsx';
+import "@/lib/components/uiChips/typewriter/styles/animatedCursor.uiChips.styles.css";
+import clsx from "clsx";
 
 interface AnimatedCursorProps extends React.HTMLAttributes<HTMLSpanElement> {
     isBlinking?: boolean;
@@ -10,29 +10,27 @@ interface AnimatedCursorProps extends React.HTMLAttributes<HTMLSpanElement> {
 /**
  * Animated cursor component for typewriter effects.
  *
- * Displays a blinking cursor character (default: "|") that can be
- * toggled on/off and made visible/invisible.
+ * @param {boolean} [isBlinking=true] - Whether the cursor should blink
+ * @param {boolean} [isVisible=true] - Whether the cursor is visible
+ * @param {string} [cursor="|"] - The cursor character to display
+ * @param {React.HTMLAttributes<HTMLSpanElement>} props - Additional HTML span attributes
  *
  * @example
- * <AnimatedCursor isBlinking={true} isVisible={isTyping} />
- *
- * @example
- * // Custom cursor character
  * <AnimatedCursor cursor="_" isBlinking={true} />
  */
 function AnimatedCursor({
     isBlinking = true,
     isVisible = true,
-    cursor = '|',
+    cursor = "|",
     className,
     ...props
 }: AnimatedCursorProps) {
     return (
         <span
             className={clsx(
-                'inline-block w-[1ch]',
-                isBlinking && 'animate-[cursor-blink_1s_steps(1)_infinite]',
-                !isVisible && 'opacity-0',
+                "inline-block w-[1ch]",
+                isBlinking && "animate-[cursor-blink_1s_steps(1)_infinite]",
+                !isVisible && "opacity-0",
                 className,
             )}
             data-blinking={isBlinking}
@@ -40,11 +38,11 @@ function AnimatedCursor({
             aria-hidden="true"
             {...props}
         >
-            {isVisible ? cursor : ''}
+            {isVisible ? cursor : ""}
         </span>
     );
 }
 
-AnimatedCursor.displayName = 'AnimatedCursor';
+AnimatedCursor.displayName = "AnimatedCursor";
 
 export default AnimatedCursor;
